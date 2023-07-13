ca8dda27f7653cf35bf01698e331c479fead9d46
uzential <bogdan.pechounov@outlook.com>
2023-07-13 10:33:19 -0400

    init

---
+++ RealmTemplate/RealmTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTemplateApp.swift
+//  RealmTemplate
+//
+//  Created by Bogdan on 2023-07-13.
+//
+
+import SwiftUI
+
+@main
+struct RealmTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
